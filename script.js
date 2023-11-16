e69242fccb00d2a7f0dc39886f75c31c53a388b7
IsharaParanagamaGedara <sulani194@gmail.com>
2023-11-16 19:12:19 +0530

    Add interactive quotes about age can add a nice touch

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const ageCalculatorForm = document.getElementById('ageCalculatorForm');
 const result = document.getElementById('result');
 
-ageCalculatorForm.addEventListener('submit', function(event) {
+// Array of quotes about age
+const ageQuotes = [
+  "Age is merely the number of years the world has been enjoying you.",
+  "You're not getting older, you're getting wiser.",
+  "Age is an issue of mind over matter. If you don't mind, it doesn't matter.",
+  "Growing old is mandatory, but growing up is optional.",
+  "Age is the opportunity to live a full life, not just accumulate years.",
+  // Add more quotes as needed
+];
+
+ageCalculatorForm.addEventListener('submit', function (event) {
   event.preventDefault();
   const dob = new Date(ageCalculatorForm.dob.value);
   const ageInMilliseconds = Date.now() - dob.getTime();
   const ageDate = new Date(ageInMilliseconds);
   const age = Math.abs(ageDate.getUTCFullYear() - 1970);
 
-  result.textContent = `Your age is ${age} years.`;
+  // Get a random quote
+  const randomQuote = ageQuotes[Math.floor(Math.random() * ageQuotes.length)];
+
+  // Display the age and a random quote
+  result.innerHTML = `<p>Your age is ${age} years.</p><p>${randomQuote}</p>`;
 });
 
 function clearFields() {
   ageCalculatorForm.reset();
-  result.textContent = '';
+  result.innerHTML = '';
 }
-
-
